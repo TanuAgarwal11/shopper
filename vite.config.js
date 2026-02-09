798a0a7417d8804d5768157f46c051a5307830a3
Tanu Agarwal <agarwaltanu564@gmail.com>
2026-02-09 19:30:01 +0530

    add changes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/shopper/", // <--- Add your repo name here (case-sensitive)
   plugins: [react()],
+  base: "/shopper/",   // EXACT repo name (lowercase)
 });
+
